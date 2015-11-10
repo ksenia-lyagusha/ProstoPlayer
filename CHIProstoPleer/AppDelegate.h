fa2a4c50881becddec27b6bef72fcf39ff5e4427
CHI Software <chisoftware@CHI-mac-mini.local>
2015-11-10 17:21:27 +0200

    Start new project

---
+++ CHIProstoPleer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProstoPleerApp
+//
+//  Created by CHI Software on 11/10/15.
+//  Copyright © 2015 CHI Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CHIProstoPleer/Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProstoPleerApp
+//
+//  Created by CHI Software on 11/10/15.
+//  Copyright © 2015 CHI Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
